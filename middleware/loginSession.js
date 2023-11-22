e272a48b06328b8d355eb1f39208962260c6e345
Bhim Prasad Lamichhane <dreamypd73@gmail.com>
2023-11-22 08:49:42 +0545

    some minor changes

--- middleware/loginSession.js
+++ middleware/loginSession.js
@@ -1,5 +1,5 @@
 exports.loginSession = (req, res, next) => {
-    const sessionTimeout = 10 * 60 * 1000; // 2 minutes
+    const sessionTimeout = 10 * 60 * 1000; 
     let timer;
   
     const clearTimer = () => {
@@ -10,14 +10,11 @@ exports.loginSession = (req, res, next) => {
   
     const resetTimer = () => {
       clearTimer();
-      // Set the timer again for the session timeout
       timer = setTimeout(() => {
-        // Clear the session and send a response indicating timeout
         req.session.destroy((err) => {
           if (err) {
             console.error("Error destroying session:", err);
           }
-          // Check if the response has been sent already
           if (!res.headersSent) {
             res.status(401).json({ message: "Session timeout" });
           }
@@ -29,11 +26,9 @@ exports.loginSession = (req, res, next) => {
       return res.status(401).json({ message: "You must be logged in to access this" });
     }
   
-    // User is logged in, reset the session timeout
     resetTimer();
     next();
   
-    // Attach event listeners for various activities that reset the timer
     req.on("data", resetTimer);
     req.on("end", resetTimer);
   };
